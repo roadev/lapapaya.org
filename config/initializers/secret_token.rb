fcd9abe8b3074b4f4716ea94cffaf358ff22d7fa
blueterrain <admin@lapapaya.org>
2014-04-06 17:56:40 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lapapayanetwork::Application.config.secret_key_base = '4006ec6f6c1cbdf4c46e39c3e3223c51dc675d87a686e0e32f32d3bb6b4f8c17947d6a4c86f8f5ce2f205ab19dcfcf489cde038c5f7fc1bae4e543d69001bba9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lapapayanetwork::Application.config.session_store :cookie_store, key: '_lapapayanetwork_session'
