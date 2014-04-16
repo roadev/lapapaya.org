bce2d37207a74cae53a55eab331ce8b058c98117
blueterrain <admin@lapapaya.org>
2014-04-15 21:18:39 -0500

    new look and feel dreams

--- app/views/dreams/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@dreams) do |dream|
-  json.extract! dream, :id, :dream, :whant, :offer, :need
-  json.url dream_url(dream, format: :json)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414220104) do
+ActiveRecord::Schema.define(version: 20140415011355) do
 
   create_table "dreams", force: true do |t|
     t.string   "dream"
