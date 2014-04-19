b41f2bb86d057d7d55121a06c4a4472d7186059d
blueterrain <admin@lapapaya.org>
2014-04-19 16:42:02 -0500

    fixed stylesheet error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Lapapayanetwork::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile = %w{application.js}
 
   # Generate digests for assets URLs.
   config.assets.digest = true
