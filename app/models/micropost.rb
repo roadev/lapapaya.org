fadacbb1175e1f36f0966ca04a0ab90a2196914c
blueterrain <admin@lapapaya.org>
2014-11-21 18:32:59 -0500

    last changes to micropost scaffold

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	attr_accessor :image_file_name
 end
