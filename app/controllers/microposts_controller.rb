823fb126cedae799fea15118173cc39e5e56cc5f
blueterrain <admin@lapapaya.org>
2014-11-25 08:11:24 -0500

    new header limpio

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -69,7 +69,7 @@ class MicropostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
-      params.require(:micropost).permit(:content, :image,)
+      params.require(:micropost).permit(:content, :image, :user_id)
 
     end
 end
