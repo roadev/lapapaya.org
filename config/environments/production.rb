a305388ea373f32dd9f9b734a8370114b559fa50
blueterrain <admin@lapapaya.org>
2015-06-15 06:32:53 -0500

    code in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,14 @@ Lapapayanetwork::Application.configure do
    }
  }
 
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           =>  587,
+  :authentication => :plain,
+  :user_name      => ENV['app23950605@heroku.com'],
+  :password       => ENV['udft7znt2980'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
 end
