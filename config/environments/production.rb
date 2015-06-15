ad415441d3410ddcc0243f7f25d377b4557304d8
blueterrain <admin@lapapaya.org>
2015-06-15 06:38:04 -0500

    puerto 80 en enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,5 @@ Lapapayanetwork::Application.configure do
    }
  }
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           =>  587,
-  :authentication => :plain,
-  :user_name      => ENV['app23950605@heroku.com'],
-  :password       => ENV['udft7znt2980'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
 
 end
