c413b9bd84103b28554f5dac982234dc04925501
blueterrain <admin@lapapaya.org>
2015-06-14 22:26:38 -0500

    puerto 80

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Lapapayanetwork::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '80',
+  :authentication => :plain,
+  :user_name      => ENV['app23950605@heroku.com'],
+  :password       => ENV['udft7znt2980'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
