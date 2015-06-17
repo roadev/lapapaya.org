223afb248a53dbecd95a07a7edd14d52c3ae1488
blueterrain <admin@lapapaya.org>
2015-06-17 05:00:00 -0500

    new order in code /production.rb lapapaya

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,14 @@ Lapapayanetwork::Application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
    }
  }
- 
+
 config.action_mailer.default_url_options = { :host => 'lapapaya.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
     :user_name =>      ENV['app23950605@heroku.com'],
     :password =>       ENV['MhLw2wx-VMiLNqZO-0l3Iw'],
-    :domain =>         'lapapay.herokuapp.com',
+    :domain =>         'lapapaya.herokuapp.com',
     :authentication => :plain
 }
 ActionMailer::Base.delivery_method = :smtp
