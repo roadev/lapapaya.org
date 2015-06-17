8f1ca36f058eb0386f067ebfc44daf1128132922
blueterrain <admin@lapapaya.org>
2015-06-17 05:07:15 -0500

    host lapapaya

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,13 @@ Lapapayanetwork::Application.configure do
    }
  }
 
-config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'lapapaya.org' }
 ActionMailer::Base.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
     :user_name =>      ENV['app23950605@heroku.com'],
     :password =>       ENV['MhLw2wx-VMiLNqZO-0l3Iw'],
-    :domain =>         'herokuapp.com',
+    :domain =>         'lapapaya.org',
     :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
