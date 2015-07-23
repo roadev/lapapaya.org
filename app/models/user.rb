d0f14ecb834b2faea471a5796115faf8fbb11b00
Juan Roa <jdroa92@gmail.com>
2015-07-23 15:26:10 -0500

    ails g merit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_merit
+
 include GiocoResource
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
