4c6454c6fe913f2d8814f5422b400e6e50e75cb7
Juan Roa <jdroa92@gmail.com>
2015-08-01 18:23:12 -0500

    Added image attachment migration to solution model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801222404) do
+ActiveRecord::Schema.define(version: 20150801232239) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -210,6 +210,10 @@ ActiveRecord::Schema.define(version: 20150801222404) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "solutions", ["user_id"], name: "index_solutions_on_user_id"
