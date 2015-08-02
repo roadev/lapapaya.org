1b5f4bf449c6d309cff2d5b35d0148f077ef292c
Juan Roa <jdroa92@gmail.com>
2015-08-01 20:12:42 -0500

    Changing user_details migration

--- db/migrate/20150801213038_add_user_details_to_user.rb
+++ db/migrate/20150801213038_add_user_details_to_user.rb
@@ -1,4 +1,14 @@
 class AddUserDetailsToUser < ActiveRecord::Migration
+  def up
+    add_column :users, :username, :string
+    add_column :users, :facebook, :string
+    add_column :users, :twitter, :string
+    add_column :users, :country, :string
+    add_column :users, :region, :string
+    add_column :users, :city, :string
+    add_column :users, :born_date, :date
+    add_column :users, :website, :string
+  end
   def change
     add_column :users, :name, :string
     add_column :users, :lastname, :string
@@ -10,5 +20,15 @@ class AddUserDetailsToUser < ActiveRecord::Migration
     add_column :users, :city, :string
     add_column :users, :born_date, :date
     add_column :users, :website, :string
+  end
+  def down
+    remove_column :users, :username, :string
+    remove_column :users, :facebook, :string
+    remove_column :users, :twitter, :string
+    remove_column :users, :country, :string
+    remove_column :users, :region, :string
+    remove_column :users, :city, :string
+    remove_column :users, :born_date, :date
+    remove_column :users, :website, :string
   end
 end
