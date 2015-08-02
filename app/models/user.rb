b1708e21ed9780fc33a39d2a53af8d36992d05b5
Juan Roa <jdroa92@gmail.com>
2015-08-01 23:08:58 -0500

    Setting devise messages for register form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :solutions
   has_attached_file :avatar, styles: { :medium => "100x100#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: %w(image/jpeg image/jpg image/png image/gif), message: 'Ingresa tu avatar en extensión jpg o png'
+  validates_uniqueness_of :username, message: ": Este nombre de usuario ya se encuentra registrado"
 include GiocoResource
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
