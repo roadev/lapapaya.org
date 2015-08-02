c15b84bcc1da7db54db9fff2e37bba02c594b10e
Juan Roa <jdroa92@gmail.com>
2015-08-01 23:17:21 -0500

    Assets precompiled

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { :medium => "100x100#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: %w(image/jpeg image/jpg image/png image/gif), message: 'Ingresa tu avatar en extensión jpg o png'
   validates_uniqueness_of :username, message: ": Este nombre de usuario ya se encuentra registrado"
+  validates :country, presence: { message: ": Por favor ingresa el país en donde vives" }
 include GiocoResource
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
