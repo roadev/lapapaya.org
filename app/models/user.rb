860aff34e6dcfddd227da3f019677701c8529055
Juan Roa <jdroa92@gmail.com>
2015-08-02 16:25:04 -0500

    Removed username and country validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { :medium => "100x100#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: %w(image/jpeg image/jpg image/png image/gif), message: 'Ingresa tu avatar en extensión jpg o png'
   validates_uniqueness_of :username, message: ": Este nombre de usuario ya se encuentra registrado"
-  validates :country, presence: { message: ": Por favor ingresa el país en donde vives" }
-  validates :username, presence: { message: ": Por favor ingresa un nombre de usuario" }
+  #validates :country, presence: { message: ": Por favor ingresa el país en donde vives" }
+  #validates :username, presence: { message: ": Por favor ingresa un nombre de usuario" }
 
   #def mydreams
   #  Dream.where(user_id: 7).count
