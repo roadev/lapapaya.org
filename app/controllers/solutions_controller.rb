43896f4234e36a77cd58f6e4529a052355558bad
Juan Roa <jdroa92@gmail.com>
2015-08-01 23:13:01 -0500

    Added spanish messages notifications for solutions and microposts

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,5 +1,6 @@
 class SolutionsController < ApplicationController
   before_action :set_solution, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /solutions
   # GET /solutions.json
@@ -28,7 +29,7 @@ class SolutionsController < ApplicationController
 
     respond_to do |format|
       if @solution.save
-        format.html { redirect_to @solution, notice: 'Solution was successfully created.' }
+        format.html { redirect_to @solution, notice: '¡ La solución ha sido enviada !' }
         format.json { render action: 'show', status: :created, location: @solution }
       else
         format.html { render action: 'new' }
@@ -42,7 +43,7 @@ class SolutionsController < ApplicationController
   def update
     respond_to do |format|
       if @solution.update(solution_params)
-        format.html { redirect_to @solution, notice: 'Solution was successfully updated.' }
+        format.html { redirect_to @solution, notice: '¡ Has editado la solución !' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -70,5 +71,12 @@ class SolutionsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def solution_params
       params.require(:solution).permit(:micropost_id, :content, :image)
+    end
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      #render text: ':(' if @micropost.nil?
+      flash[:danger] = "Este contenido sólo puede modificarlo o eliminarlo el propietario." if @micropost.nil?
+      flash[:success] = "La solución ha sido Eliminada." if @micropost.present?
+      redirect_to microposts_url if @micropost.nil?
     end
 end
