60767662eb7337de1370097ead893dd627512c2d
Juan Roa <jdroa92@gmail.com>
2015-08-03 10:39:50 -0500

    Changed mandrill params

--- config/application.rb
+++ config/application.rb
@@ -24,10 +24,10 @@ module Lapapayanetwork
     # Required for heroku
     config.assets.initialize_on_precompile = false
     ActionMailer::Base.smtp_settings = {
-        :port =>           '2525',
+        :port =>           '587',
         :address =>        'smtp.mandrillapp.com',
         :user_name =>      "app23950605@heroku.com",
-        :password =>       "6RxMIvoslDILJ9nGoihpjA",
+        :password =>       "MhLw2wx-VMiLNqZO-0l3Iw",
         :authentication => :plain
     }
   end
--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,9 @@ Lapapayanetwork::Application.initialize!
 ActionMailer::Base.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['app23950605@heroku.com'],
-    :password =>       ENV['MhLw2wx-VMiLNqZO-0l3Iw'],
-    :domain =>         'heroku.com',
+    :user_name =>      "app23950605@heroku.com",
+    :password =>       "MhLw2wx-VMiLNqZO-0l3Iw",
+    :domain =>         'lapapaya.org',
     :authentication => :plain
 }
 ActionMailer::Base.delivery_method = :smtp
