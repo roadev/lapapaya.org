b98062606501fc4dc09d14011c5f7faeff6ff1bd
Juan Roa <jdroa92@gmail.com>
2015-08-02 22:49:32 -0500

    Fixing mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Lapapayanetwork::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['app23950605@heroku.com'],
-    :password =>       ENV['MhLw2wx-VMiLNqZO-0l3Iw'],
+    :user_name =>      'app23950605@heroku.com',
+    :password =>       MhLw2wx-VMiLNqZO-0l3Iw,
     :domain =>         'lapapaya.org',
     :authentication => :plain,
   }
