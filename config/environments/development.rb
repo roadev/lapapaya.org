e6b740395083c07e824f45d1da68ad7f3882efbe
Juan Roa <jdroa92@gmail.com>
2015-08-03 08:25:23 -0500

    Trying with mailgunner2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Lapapayanetwork::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'lapapaya.org' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
         api_key: 'key-3401bbf966623d1cd0b9536aa4866b96',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Lapapayanetwork::Application.configure do
     }
   }
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'lapapaya.org' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
         api_key: 'key-3401bbf966623d1cd0b9536aa4866b96',
