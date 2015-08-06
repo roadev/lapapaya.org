a1adec6446a5dde103060129703cc1275154c2e4
Juan Roa <jdroa92@gmail.com>
2015-08-06 14:26:45 -0500

    Trying with account update + ip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :dreams
   has_many :microposts
   has_many :solutions
-  geocoded_by :address
+  geocoded_by :current_sign_in_ip
   #geocoded_by :address
   after_validation :geocode
   after_update :geocode
