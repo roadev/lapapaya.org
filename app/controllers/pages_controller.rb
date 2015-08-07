dad56269e19d707ae56c2845c97a8d596a8159ae
Juan Roa <jdroa92@gmail.com>
2015-08-07 11:36:44 -0500

    Showing users location working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def userpanel
+    @users = User.all
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
     marker.lat user.latitude
     marker.lng user.longitude
+    #marker.infowindow
 end
   end
   def patrocinios
