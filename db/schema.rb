b50da7dde08d9809f6ee765c227f4c442677e614
Juan Roa <jdroa92@gmail.com>
2015-09-18 15:25:41 -0500

    Migrating

--- db/schema.rb
+++ db/schema.rb
@@ -279,6 +279,9 @@ ActiveRecord::Schema.define(version: 20150914220027) do
     t.string   "city"
     t.date     "born_date"
     t.string   "website"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
