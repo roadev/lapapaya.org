b55ce5f47dd7500482450763ac797f1d44d47257
Juan Roa <jdroa92@gmail.com>
2015-10-16 14:43:30 -0500

    Basic notification and template working for complice model

--- app/models/complice.rb
+++ app/models/complice.rb
@@ -1,3 +1,9 @@
 class Complice < ActiveRecord::Base
   belongs_to :user
+  belongs_to :dream
+  after_create :send_email
+
+  def send_email
+    CompliceCreation.complice_email(self.user, self.id).deliver
+  end
 end
