ad32afe54a8c7cd1eb08985b2ff5b22403608dbe
Juan Roa <jdroa92@gmail.com>
2015-10-22 14:55:32 -0500

    Mailer submit for complice fixed

--- app/mailers/complice_creation.rb
+++ app/mailers/complice_creation.rb
@@ -2,11 +2,11 @@ class CompliceCreation < ActionMailer::Base
      default from: "admin@lapapaya.org"
      layout "mailer"
    def complice_email(user, complice)
-    @complice = complice
+    #@complice = complice
     @name_user=User.find(user).name
     @lastname_user=User.find(user).lastname
     @dream_number = Complice.find(complice).dream_id
 
-    mail(to: "#{user.name} <#{user.email}>", subject: "¡Has apoya un sueño!")
+    mail(to: "#{user.name} <#{user.email}>", subject: "¡Has apoyado un sueño!")
    end
  end
