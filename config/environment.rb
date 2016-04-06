8859c21267cad5089f21bd9dfb9d62d48cbe59ef
Juan Roa <jdroa92@gmail.com>
2016-04-06 10:40:39 -0500

    Refactoring smtp credentials config

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Lapapayanetwork::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      "app23950605@heroku.com",
-    :password =>       "MhLw2wx-VMiLNqZO-0l3Iw",
-    :domain =>         'lapapaya.org',
-    :authentication => :plain
-}
-ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,15 @@ Lapapayanetwork::Application.configure do
     }
   }
 
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+      port:           '587',
+      address:        ENV["MAILGUN_SMTP_SERVER"],
+      user_name:      ENV["MAILGUN_SMTP_LOGIN"],
+      password:       ENV["MAILGUN_SMTP_PASSWORD"],
+      authentication: :plain
+  }
+
   config.action_mailer.default_url_options = { host: 'lapapaya.org' }
 end
