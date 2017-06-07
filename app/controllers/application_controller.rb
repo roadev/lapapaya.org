aa7310db1176bd556d3350f69178d4781aecddf8
Juan Roa <jdroa92@gmail.com>
2017-06-07 11:35:21 -0500

    Adding allowed params in devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-    user_params.permit(:username, :email)
+  def configure_permitted_parameters
+    params = [:name, :lastname, :username, :phone, :country, :city,
+      :born_date, :website, :facebook, :twitter, :avatar]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: params)
+    devise_parameter_sanitizer.permit(:account_update, keys: params)
   end
-end
 
   def last_dream
     Dream.where( user_id: current_user.id ).last
